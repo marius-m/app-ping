88b7d02503dba0671d235027a937f2ef556ad989
Marius Merkevicius <marius.m@ito.lt>
2022-09-01 11:13:29 +0300

    Ping endpoint

--- ws/src/main/kotlin/lt/markmerkk/ping/controllers/BasicRestController.kt
+++ ws/src/main/kotlin/lt/markmerkk/ping/controllers/BasicRestController.kt
@@ -3,8 +3,11 @@ package lt.markmerkk.ping.controllers
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,13 +19,16 @@ open class BasicRestController {
     lateinit var objectMapper: ObjectMapper
 
     @RequestMapping(
-        value = ["/hello"],
-        method = [RequestMethod.GET],
+        value = ["/ping"],
+        method = [RequestMethod.POST],
         produces = ["application/json"]
     )
     @ResponseBody
-    fun ping(): String {
-        return "hello"
+    fun ping(
+        @RequestBody(required = false) content: String?,
+    ): HttpStatus {
+        l.info("ping(content: '{}')", content)
+        return HttpStatus.OK
     }
 
     companion object {
