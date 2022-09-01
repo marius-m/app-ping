6c5fe79655ba44fc1eb4f770704cd58fd5c027c8
Marius Merkevicius <marius.m@ito.lt>
2022-09-01 11:39:01 +0300

    More info in request to resolve user

--- ws/src/main/kotlin/lt/markmerkk/ping/controllers/BasicRestController.kt
+++ ws/src/main/kotlin/lt/markmerkk/ping/controllers/BasicRestController.kt
@@ -5,11 +5,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,9 +26,10 @@ open class BasicRestController {
     )
     @ResponseBody
     fun ping(
+        @RequestHeader(value = "User-Agent", required = false) userAgent: String?,
         @RequestBody(required = false) content: String?,
     ): HttpStatus {
-        l.info("ping(content: '{}')", content)
+        l.info("ping(userAgent: '{}', content: '{}')", userAgent, content)
         return HttpStatus.OK
     }
 
