faddd96b7b6ad71d7851c2685ae9ffae5c229720
Marius Merkevicius <marius.m@ito.lt>
2022-09-07 12:16:40 +0300

    Update endpoint

--- ws/src/main/kotlin/lt/markmerkk/ping/entities/requests/ContentDetailRequest.kt
+++ ws/src/main/kotlin/lt/markmerkk/ping/entities/requests/ContentDetailRequest.kt
@@ -3,7 +3,6 @@ package lt.markmerkk.ping.entities.requests
 data class ContentDetailRequest(
     val coordLat: Double,
     val coordLong: Double,
-    val dtLastPing: String,
     val dtCurrent: String,
-    val extras: String,
+    val extras: String? = "",
 )
